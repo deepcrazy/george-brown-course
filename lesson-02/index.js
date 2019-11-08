f4a893e51c355d77178ddeba10baddc2ceee06ed
deepcrazy <312.gupta@gmail.com>
2019-11-07 21:58:54 -0500

    completed exercise-2 and installed eslint and prettier formatter

--- lesson-02/index.js
+++ lesson-02/index.js
@@ -1,11 +1,12 @@
-import * as mdc from 'material-components-web';
+import * as mdc from 'material-components-web'
 
 const textFieldElements = document.querySelectorAll('.mdc-text-field')
 for (let textFieldElement of textFieldElements) {
-  const textField = new mdc.textField.MDCTextField(textFieldElement);
+  const textField = new mdc.textField.MDCTextField(textFieldElement)
 }
 
-const submitButtonElement = document.getElementById('submit-button'); submitButtonElement.addEventListener('click', onClickSubmit)
+const submitButtonElement = document.getElementById('submit-button')
+submitButtonElement.addEventListener('click', onClickSubmit)
 
 function onClickSubmit() {
   const { elements } = submitButtonElement.form
@@ -18,37 +19,37 @@ function onClickSubmit() {
 
     if (element.type === 'radio') {
       if (element.checked) {
-        data.set(element.name, element.id);
-        continue;
+        data.set(element.name, element.id)
+        continue
       }
       continue
     }
 
     if (element.type === 'checkbox') {
-      data.set(element.id, element.checked);
+      data.set(element.id, element.checked)
       continue
     }
 
     if (element.type === 'text') {
       if (element.value === '') {
-        const section = element.closest('.Container_Section');
+        const section = element.closest('.Container_Section')
         showError(section)
-        removeErrorOnTextInput(section, element);
+        removeErrorOnTextInput(section, element)
         return
       }
       data.set(element.id, element.value)
       continue
     }
 
-    throw new Error(`Unrecognized element type: ${element.type}`);
+    throw new Error(`Unrecognized element type: ${element.type}`)
   }
 
   if (!data.get('diet-restrictions')) {
     const element = document.querySelector('[name="diet-restrictions"]')
     const section = element.closest('.Container_Section')
-    showError(section);
-    removeErrorOnRadioChange(section, element);
-    return;
+    showError(section)
+    removeErrorOnRadioChange(section, element)
+    return
   }
 
   console.log('Data: %o', data)
@@ -57,7 +58,9 @@ function onClickSubmit() {
 
 function displayData(data) {
   const formOutputElement = document.getElementById('form-output')
-  if (!formOutputElement) { return; }
+  if (!formOutputElement) {
+    return
+  }
   const dataAsObject = Object.fromEntries(data.entries())
   formOutputElement.innerHTML = `
     <p>Output data:</p>
@@ -66,8 +69,6 @@ function displayData(data) {
 }
 
 function showError(section) {
-
-
   section.classList.add('Container_Section__Error')
   setTimeout(() => alert('Please fill in a value for the field'), 100)
 }
@@ -86,3 +87,17 @@ function removeErrorOnRadioChange(section, element) {
 function removeError(section) {
   section.classList.remove('Container_Section__Error')
 }
+
+const screenSizeButton = document.getElementById('screen-size-button')
+screenSizeButton.addEventListener('click', getAvailedScreenSize)
+
+function getAvailedScreenSize() {
+  event.preventDefault()
+  const availedScreenSize = document.getElementById('availed-screen-size')
+  if (!availedScreenSize) {
+    return
+  }
+  availedScreenSize.innerHTML = window.innerWidth + ' X ' + window.innerHeight
+}
+
+console.dir(document.body)
