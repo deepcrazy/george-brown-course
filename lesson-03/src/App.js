e754b3ffd976e1d4d2797deedc3bb67c7b18219e
deepcrazy <312.gupta@gmail.com>
2019-11-14 17:32:10 -0500

    Completed Exercise-2 for Lesson-3

--- lesson-03/src/App.js
+++ lesson-03/src/App.js
@@ -1,21 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css'
 
 export default function App() {
+
+  const [firstName, setFirstName] = useState("Deepanshu");
+  const [lastName, setLastName] = useState("Gupta");
+  const [isAbove19, setAbove19] = useState(false);
+  const [dietRestriction, setDietRestriction] = useState('None');
+  const [currentYear, setCurrentYear] = useState(null);
+
+  function handleFirstNameChange (e) {
+    setFirstName(e.target.value)
+  }
+
+  function handleLastNameChange (e) {
+    setLastName(e.target.value)
+  }
+
+  function handleAbove19CheckboxClick(e) {
+    setAbove19(e.target.checked)
+  }
+
+  function handleRadioClick(e) {
+    setDietRestriction(e.target.value)
+  }
+
+  function handleCurrentYear(e){
+    const nextYear = Number(e.target.value);
+    if (Number.isFinite(nextYear)) {
+      setCurrentYear(nextYear)
+    }
+  }
+
   return (
     <div className='App'>
       <div className='App-content'>
         <h1>Registration Form</h1>
         {/*=== Your content goes here 👇 ===*/}
+        
         <form>
           <label>
             First Name:
-            <input type='text' name='name' defaultValue='Deepanshu' />
+            <input type="text" onChange={e =>  handleFirstNameChange(e)} value={firstName}></input>
+            {firstName}
           </label>
           <br></br>
           <label>
             Last Name:
-            <input type='text' name='name' defaultValue='Gupta' />
+            <input type="text" onChange={handleLastNameChange} value={lastName}></input>
+            {lastName}
           </label>
           <br></br>
           <input type='checkbox'></input>
@@ -28,26 +61,26 @@ export default function App() {
             <div>
               <label>
                 Vegetarian
-                <input type='radio'></input>
+                <input type='radio' name="diet" value="vegetarian" onClick={handleRadioClick}></input>
               </label>
               <label>
                 Vegan
-                <input type='radio'></input>
+                <input type='radio' name="diet" value="vegan" onClick={handleRadioClick}></input>
               </label>
               <label>
                 Halal/Kosher
-                <input type='radio'></input>
+                <input type='radio' name="diet" value="halal/koshar" onClick={handleRadioClick}></input>
               </label>
               <label>
                 None
-                <input type='radio'></input>
+                <input type='radio' name="diet" value="None" onClick={handleRadioClick}></input>
               </label>
             </div>
           </div>
           <br></br>
           <label>
             Current Year
-            <input type='text' defaultValue='2019'></input>
+            <input type='text' onChange={handleCurrentYear} value={currentYear}></input>
           </label>
         </form>
       </div>
