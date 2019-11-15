c007f50cd035729f30efd78e04f8e74ffd44060e
deepcrazy <312.gupta@gmail.com>
2019-11-14 20:13:20 -0500

    Exercise-1 completed for Lesson-04

--- lesson-04/src/App.js
+++ lesson-04/src/App.js
@@ -1,12 +1,51 @@
 import React from 'react'
 import './App.css'
 
-export default function App() {
-  const [firstName, setFirstName] = React.useState('')
-  const onChangeFirstName = event => setFirstName(event.target.value)
+function useCustomHook() {
+  const [value, setValue] = React.useState('')
+  const onChange = event => setValue(event.target.value)
+  return {
+    value,
+    onChange,
+  }
+}
 
-  const [lastName, setLastName] = React.useState('')
-  const onChangeLastName = event => setLastName(event.target.value)
+function InputField({ inputClassName, type, placeholder, labelText, value, onChange }) {
+  return (
+    <label className='FormField-Label'>
+      <span className='FormField-LabelText'>{labelText}</span>
+      <input
+        className={inputClassName}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  )
+}
+
+function RadioField({ type, value, checked, onChange, labelText }) {
+  return (
+    <label className='FormField-Label FormField-Label__Radio'>
+      <span className='FormField-LabelText FormField-LabelText__Radio'>
+        <input
+          className='FormField-Input FormField-Input__Radio'
+          type={type}
+          value={value}
+          checked={checked}
+          onChange={onChange}
+        />
+        {labelText}
+      </span>
+    </label>
+  )
+}
+
+export default function App() {
+
+  const firstNameState = useCustomHook()
+  const lastNameState = useCustomHook()
 
   const [isAbove19, setIsAbove19] = React.useState(false)
   const onChangeAbove19 = event => setIsAbove19(event.target.checked)
@@ -24,29 +63,27 @@ export default function App() {
         <h1 className='App-Title'>Registration Form</h1>
 
         <div className='FormField'>
-          <label className='FormField-Label'>
-            <span className='FormField-LabelText'>First Name</span>
-            <input
-              className='FormField-Input FormField-Input__Text'
-              type='text'
-              placeholder='Enter your first name'
-              value={firstName}
-              onChange={onChangeFirstName}
-            />
-          </label>
+          <InputField
+            inputClassName='FormField-Input FormField-Input__Text'
+            type='text'
+            placeholder='Enter your first name'
+            labelText='First Name'
+            value={firstNameState.value}
+            onChange={firstNameState.onChange}
+          />
+          {firstNameState.value}
         </div>
 
         <div className='FormField'>
-          <label className='FormField-Label'>
-            <span className='FormField-LabelText'>Last Name</span>
-            <input
-              className='FormField-Input FormField-Input__Text'
-              type='text'
-              placeholder='Enter your last name'
-              value={lastName}
-              onChange={onChangeLastName}
-            />
-          </label>
+          <InputField
+            inputClassName='FormField-Input FormField-Input__Text'
+            type='text'
+            placeholder='Enter your last name'
+            labelText='Last Name'
+            value={lastNameState.value}
+            onChange={lastNameState.onChange}
+          />
+          {lastNameState.value}
         </div>
 
         <div className='FormField'>
@@ -66,57 +103,37 @@ export default function App() {
         <div className='FormField'>
           <div className='FormField-Heading'>Diet Restriction</div>
 
-          <label className='FormField-Label FormField-Label__Radio'>
-            <span className='FormField-LabelText FormField-LabelText__Radio'>
-              <input
-                className='FormField-Input FormField-Input__Radio'
-                type='radio'
-                value='vegetarian'
-                checked={diet === 'vegetarian'}
-                onChange={onChangeDiet}
-              />
-              Vegetarian
-            </span>
-          </label>
+          <RadioField
+            type='radio'
+            value='vegetarian'
+            checked={diet === 'vegetarian'}
+            onChange={onChangeDiet}
+            labelText='Vegetarian'
+          />
 
-          <label className='FormField-Label FormField-Label__Radio'>
-            <span className='FormField-LabelText FormField-LabelText__Radio'>
-              <input
-                className='FormField-Input FormField-Input__Radio'
-                type='radio'
-                value='vegan'
-                checked={diet === 'vegan'}
-                onChange={onChangeDiet}
-              />
-              Vegan
-            </span>
-          </label>
+          <RadioField
+            type='radio'
+            value='vegan'
+            checked={diet === 'vegan'}
+            onChange={onChangeDiet}
+            labelText='Vegan'
+          />
 
-          <label className='FormField-Label FormField-Label__Radio'>
-            <span className='FormField-LabelText FormField-LabelText__Radio'>
-              <input
-                className='FormField-Input FormField-Input__Radio'
-                type='radio'
-                value='halal-kosher'
-                checked={diet === 'halal-kosher'}
-                onChange={onChangeDiet}
-              />
-              Halal/Kosher
-            </span>
-          </label>
+          <RadioField
+            type='radio'
+            value='halal-kosher'
+            checked={diet === 'halal-kosher'}
+            onChange={onChangeDiet}
+            labelText='Halal/Kosher'
+          />
 
-          <label className='FormField-Label FormField-Label__Radio'>
-            <span className='FormField-LabelText FormField-LabelText__Radio'>
-              <input
-                className='FormField-Input FormField-Input__Radio'
-                type='radio'
-                value='none'
-                checked={diet === 'none'}
-                onChange={onChangeDiet}
-              />
-              None
-            </span>
-          </label>
+          <RadioField
+            type='radio'
+            value='none'
+            checked={diet === 'none'}
+            onChange={onChangeDiet}
+            labelText='None'
+          />
         </div>
 
         <div className='FormSubmit'>
