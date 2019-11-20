70ccb6b3393c83347cc125bc4c5eaf06708d6065
deepcrazy <312.gupta@gmail.com>
2019-11-19 21:11:20 -0500

    Completed exercise-2 of lesson-05

--- lesson-05/src/App.js
+++ lesson-05/src/App.js
@@ -83,23 +83,23 @@ function Exercise1() {
   const [showerValue, setShowerValue] = React.useState(null)
   const onChangeShowerValue = event => setShowerValue(event.target.value)
 
-    const valuesArray = []
-    if (foodValue != null || poolValue != null || bedValue != null || showerValue != null){
-      valuesArray.push(foodValue)
-      valuesArray.push(poolValue)
-      valuesArray.push(bedValue)
-      valuesArray.push(showerValue)
-    }
+  const valuesArray = []
+  if (foodValue != null || poolValue != null || bedValue != null || showerValue != null) {
+    valuesArray.push(foodValue)
+    valuesArray.push(poolValue)
+    valuesArray.push(bedValue)
+    valuesArray.push(showerValue)
+  }
 
-    console.log(valuesArray)
-    const newArray = []
-    valuesArray.forEach((item) => {
-      if (item != null){
-        newArray.push(parseInt(item))
-      }
-    })
-    console.log(newArray)
-    const avgValue = newArray.length !== 0 ? newArray.reduce((a, b) => a + b) / newArray.length : ""
+  // console.log(valuesArray)
+  const newArray = []
+  valuesArray.forEach((item) => {
+    if (item != null) {
+      newArray.push(parseInt(item))
+    }
+  })
+  // console.log(newArray)
+  const avgValue = newArray.length !== 0 ? newArray.reduce((a, b) => a + b) / newArray.length : ""
 
   return (
     <div>
@@ -141,12 +141,40 @@ function RatingValue({ onChange, name, value }) {
 
 function Exercise2() {
   // Put your answers here for Exercise 2 👇
+
+  const [remainingCount, setRemainingCount] = React.useState(3)
+  const onChange = event => {
+    if (event.target.checked)
+      setRemainingCount(remainingCount - 1)
+    else
+    setRemainingCount(remainingCount + 1)
+  }
+
+  return (
+    <div>
+    <CheckBoxItem onChange={onChange} label= 'Did you enjoy your stay?'></CheckBoxItem>
+    <CheckBoxItem onChange={onChange} label='Will you consider returning again?'></CheckBoxItem>
+    <CheckBoxItem onChange={onChange} label='Would you recommend our service?'></CheckBoxItem>
+    <div>
+      Total Remaining: {remainingCount}
+    </div>
+    </div>
+  )
+}
+
+function CheckBoxItem({label, onChange}) {
+  return (
+    <div>
+      <CheckBoxValue onChange={onChange} label={label}></CheckBoxValue>
+    </div>
+  )
+}
+
+function CheckBoxValue({ label, onChange }) {
   return (
     <label>
-      <input type="checkbox"></input>
-      <span>
-        Did you enjoy your stay?
-      </span>
+      <input onChange={onChange} type="checkbox"></input>
+      <span>{label}</span>
     </label>
   )
 }
